0cb0750668ea557e21edca1ebb1af5039fc5ed0a
Vasilij22 <vasilijbelov2@inbox.lv>
2021-09-20 13:59:49 +0300

    new file:   numbers.py

---
+++ numbers.py
@@ -0,0 +1,21 @@
+num1 = 6 #int
+num2 = 2.5 #float
+num3 = num1 + num2
+print(num3)
+print(str(num1)+ "plus",str (num2)+ "vienads ar" + str(num3))
+print(str(num1), "plus",str (num2), "vienads ar" , str(num3))
+#ievads
+num1 = int(input("Ievadi skaitli: "))
+
+num2 = int(input("Ievadi otru skaitli: "))
+
+num123 = num1 + num2
+
+
+print(num123)
+
+
+print(f"ja {num1} saskaitit ar {num2} tas bus {num123}")
+print(f"paldies  (a) ")
+
+ 
