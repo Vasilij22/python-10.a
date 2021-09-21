c09c966202a665629803d046d686b43c2d0df3e5
Vasilij22 <vasilijbelov2@inbox.lv>
2021-09-21 12:48:51 +0300

    new file:   kalkulator.py 	new file:   lists.py

---
+++ kalkulator.py
@@ -0,0 +1,16 @@
+
+f_num = int(input("Введите первое число>>"))
+oper = input("Введите операцию")
+s_num = int(input("Введите второе число>>")) 
+if oper == '+':
+  print(f_num + s_num)
+elif oper == '-':
+  print(f_num - s_num)
+elif oper == '*':
+  print(f_num * s_num)
+elif oper == '/':
+  print(f_num / s_num)
+else:
+    print("ERROR")
+zakoncic = input("Введи любую клавишу, чтобы закончить")
+print()
