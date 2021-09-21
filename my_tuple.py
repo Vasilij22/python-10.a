595ebd2f2533a811f6e6a029a09005f754c113fb
Vasilij22 <91057254+Vasilij22@users.noreply.github.com>
2021-09-21 13:55:18 +0300

    Create my_tuple.py

---
+++ my_tuple.py
@@ -0,0 +1,20 @@
+#tuples
+
+#()
+
+my_tuple = ('spelet', 'gulet', 'sportot')
+
+print(len(my_tuple))
+
+print(my_tuple[1])
+
+print(my_tuple.index('est'))
+
+my_tuple = (11,12,13,14,11,13)
+
+print(my_tuple.count(11))
+
+
+#set
+
+# {}
