4b473a23d08119e6eb50ab97d15f34867dd68ab3
Vasilij22 <91057254+Vasilij22@users.noreply.github.com>
2021-09-21 13:48:33 +0300

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,30 @@
+#mainigais biblioteka
+
+#(), katrai vertibai ir sava atslega
+
+pirmais = {'atsl1':'verti', 'atsl2':'vert2'}
+print(pirmais)
+
+print(pirmais['atsl1'])
+
+otrais = {'atsl1':[5,7,9], 'atls2':'teksts', 'atsl3':1}
+
+print(otrais['atsl1'],(7))
+
+tresais = {'macibu priekšmeti':{'gepgrafija :3,  "krievu valoda":5, "programešana":4'}}
+
+print(tresais["Macibas priekšmeti"]["krievu valoda"][2])
+
+#tukšas bibleotekas izveide
+
+pirmais = {}
+pirmais['kalni'] = 'everests'
+pirmais['udenstilpne'] = 'ezers'
+
+
+print(pirmais)
+
+print(pirmais.keys())
+print(pirmais.value())
+
+print(pirmais.item())
